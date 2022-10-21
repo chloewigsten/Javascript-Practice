8b553d34fdc52090754eaaf178a9c1ee2f2b94e5
chloewigsten <chloewigsten@gmail.com>
2022-10-21 13:24:27 -0400

    create javascript practice git

---
+++ new.js
@@ -0,0 +1,15 @@
+/*
+ * Prompt 3:
+ *
+ * Create a variable and assign a number to it. On a separate line for each:
+ *   - add a number to your variable
+ *   - subtract a number from your variable
+ *   - multiply your variable by 3
+ *   - divide your variable by 7
+ *   - calculate the remainder of dividing your variable by 2
+ */
+
+let myNumber = 29;
+
+let divider = myNumber % 2
+console.log(divider)
